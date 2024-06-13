392c055625623f58298f9f1061213131a9504e8e
iphadan <ephadaniel177@gmail.com>
2024-06-13 17:53:14 +0300

    init scraper

---
+++ remoteok_scraper.py
@@ -0,0 +1,9 @@
+import requests
+import xlwt
+from xlwt import Workbook
+import smtplib
+from os.path import basename
+from email.mime.application import MIMEApplication
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.utils import COMMASPACE, formatdate
