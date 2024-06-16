640c51d55eff4aca507792cddcaa38e324765146
iphadan <ephadaniel177@gmail.com>
2024-06-16 11:21:30 +0300

    error caused by kasperky

--- remoteok_scraper.py
+++ remoteok_scraper.py
@@ -7,3 +7,20 @@ from email.mime.application import MIMEApplication
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.utils import COMMASPACE, formatdate
+
+BASE_URL="https://remoteok.com"
+USER_AGENT="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36"
+REQUEST_HEADER={
+'User-Agent':USER_AGENT,
+'Accept-Language':'en-US, en;q=0.5'
+}
+
+def getJobPosted():
+    response=requests.get(url=BASE_URL,headers=REQUEST_HEADER,verify=False)
+    return response.content.decode("utf-8-sig")
+
+if __name__=="__main__":
+    response=getJobPosted()
+    
+    print("=======>",response)
+
